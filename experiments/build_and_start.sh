956ef6de6cad9a277ef9ea0466e53e2feb8e2e6c
harry <gavriilidis@tu-berlin.de>
2023-05-12 03:29:13 +0200

    adjusted experiments script

--- experiments/build_and_start.sh
+++ experiments/build_and_start.sh
@@ -3,22 +3,26 @@ set -x
 #params
 #$1 docker container name
 CONTAINER=$1
-#$2 1: build & run, 2: only run
+#$2 1: build & run, 2: only run, 3: only build
 OPTION=$2
 #$3 run params (for now compression library)
 RUNPARAMS=$3
 
-if [[ $OPTION == 1 ]]; then
+if [ $OPTION == 1 ] || [ $OPTION == 3 ]; then
+  DIR=$(dirname $(dirname "$(realpath -- "$0")"))
+  docker exec $CONTAINER bash -c "rm -rf xdbc-server && mkdir xdbc-server"
   #copy files
   for filetype in cpp h txt hpp; do
-    for file in ../*.$filetype; do
+    for file in ${DIR}/*.$filetype; do
       docker cp $file $CONTAINER:/xdbc-server/
-      done
     done
+  done
   #build
-  docker exec -it $CONTAINER bash -c "cd xdbc-server && rm -rf build/ && mkdir build && cd build && cmake .. && make -j8"
+  docker exec $CONTAINER bash -c "cd xdbc-server && rm -rf build/ && mkdir build && cd build && cmake .. && make -j8"
 
 fi
 
 # start
-docker exec -it $CONTAINER bash -c "cd xdbc-server/build && ./xdbc-server ${RUNPARAMS}"
+if [[ $OPTION != 3 ]]; then
+  docker exec $CONTAINER bash -c "cd xdbc-server/build && ./xdbc-server ${RUNPARAMS}"
+fi
